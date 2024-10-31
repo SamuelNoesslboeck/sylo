f02940abd3c80e4ac8456b69832baef723a0ca20
SamuelNoesslboeck <SamuelNoesslboeck>
2024-10-31 17:33:05 +0100

    types change

--- types.hpp
+++ types.hpp
@@ -1,14 +1,5 @@
 # pragma once
 
-// Helper int types
-typedef char i8;
-typedef short i16;
-typedef int i32;
-
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
-
 // Special types
 enum class Direction : bool {
     CW = true,
